474a8406222720594bae5498df0cd5a356a71a39
uf-code <ugur.firat.developer@gmail.com>
2022-06-08 00:59:20 +0300

    variables annotations

---
+++ typescript/fetchjson/annotations/types.ts
@@ -0,0 +1,10 @@
+const today: Date = new Date()
+today.getMonth()
+
+interface Person {
+    age: number
+}
+
+const person: Person = {
+    age: 123,
+}
---
+++ typescript/fetchjson/annotations/variables.ts
@@ -0,0 +1,13 @@
+let apples: number = 5
+apples = 10
+
+let speed: string = 'fast'
+speed = 'slow'
+
+let hasName: boolean = true
+
+let nothingMuch: null = null
+let nothing: undefined = undefined
+
+// built in objects
+let now: Date = new Date()
