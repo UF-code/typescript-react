74053cd3903af262a4e9cb93103b144cb41052e2
uf-code <ugur.firat.developer@gmail.com>
2022-06-08 19:02:13 +0300

    array, classes, object literal

--- typescript/fetchjson/annotations/variables.ts
+++ typescript/fetchjson/annotations/variables.ts
@@ -11,3 +11,27 @@ let nothing: undefined = undefined
 
 // built in objects
 let now: Date = new Date()
+
+// array
+// array
+// array
+let colors: string[] = ['red', 'green', 'blue']
+
+let myNumbers: number[] = [1, 2, 3, 4, 5]
+
+let truths: boolean[] = [true, true, false]
+
+// classes
+// classes
+// classes
+class Car {}
+
+let car: Car = new Car()
+
+// Object literal
+// Object literal
+// Object literal
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20,
+}
